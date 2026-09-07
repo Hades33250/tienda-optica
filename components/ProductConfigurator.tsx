b13908727b538e170ce0a6758516681894e03d46
Hades33250 <117785618+Hades33250@users.noreply.github.com>
2026-09-07 11:55:35 -0600

    Create ProductConfigurator.tsx
    Precios Micas

---
+++ components/ProductConfigurator.tsx
@@ -0,0 +1,17 @@
+| Tipo de mica                                | Precio |
+| ------------------------------------------- | ------ |
+| CR-39 monofocal con antirreflejante         | $800   |
+| CR-39 monofocal con filtro azul             | $1,200 |
+| CR-39 fotocromático con filtro azul         | $1,500 |
+| Policarbonato con antirreflejante           | $1,000 |
+| Policarbonato con filtro azul               | $1,400 |
+| Alto índice                                 | $1,500 |
+| Bifocal FT con antirreflejante              | $1,600 |
+| Bifocal FT con filtro azul                  | $1,800 |
+| Bifocal FT fotocromático con filtro azul    | $2,200 |
+| Bifocal Blend con antirreflejante           | $1,300 |
+| Bifocal Blend con filtro azul               | $1,900 |
+| Bifocal Blend fotocromático con filtro azul | $2,300 |
+| Progresivo con antirreflejante              | $1,900 |
+| Progresivo con filtro azul                  | $2,100 |
+| Progresivo fotocromático con filtro azul    | $2,500 |
